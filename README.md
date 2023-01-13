cc2d9abbb6ef2f140959dc11647715b3574116c6
Zhurkin-aa <61109852+Zhurkin-aa@users.noreply.github.com>
2023-01-13 23:09:06 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# 
+1)install python 3.7.6
+2)in comand line (anaconda better) pip install requirements.txt
+3) python main.py
---
+++ main.py
@@ -0,0 +1,11 @@
+import kivi_app
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    kivi_app.run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
